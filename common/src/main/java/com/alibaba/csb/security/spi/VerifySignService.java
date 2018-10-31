14c1b9f91267cf43192b3bc8dcf06cb7677ba1d7
tingbin.ctb <tingbin.ctb@alibaba-inc.com>
2018-10-31 10:00:17 +0800

    update to 1.1.5.1 for SM3withSM2 surport

--- common/src/main/java/com/alibaba/csb/security/spi/VerifySignService.java
+++ common/src/main/java/com/alibaba/csb/security/spi/VerifySignService.java
@@ -11,9 +11,10 @@ public interface VerifySignService {
     /**
      * 将制定的参数进行验签
      *
-     * @param paramNodeList 已签名的请求参数key=values键值对列表
+     * @param paramNodeList 已签名的请求参数key=values键值对列表，不包含签名 _api_signature 的key值。
      * @param accessKey     进行签名的凭证识别码
+     * @param signature     签名串
      */
-    String verifySignature(final List<ParamNode> paramNodeList, final String accessKey);
+    boolean verifySignature(final List<ParamNode> paramNodeList, final String accessKey, final String signature);
 
 }
